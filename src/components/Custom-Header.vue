09b67f5f7f30d793bc4192497c07272cf937c6ea
Sepehr-Aghdasi82 <Sepehr.Aghdasi82@gmail.com>
2022-06-23 13:42:17 +0430

    Complete Header.vue component

--- src/components/Custom-Header.vue
+++ src/components/Custom-Header.vue
@@ -1,7 +1,10 @@
 <template>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                  <a class="navbar-brand" href="#"
+      <nav class="navbar navbar-expand-lg navbar-light bg-light p-4">
+            <div class="container-fluid px-5">
+                  <a
+                        class="navbar-brand pe-4 border border-2 border-bottom-0 border-top-0 border-start-0"
+                        style="border-color: #1e1e1e !important"
+                        href="#"
                         ><img src="../assets/images/ANTIQUE-PIANO.png" alt=""
                   /></a>
                   <button
@@ -16,26 +19,51 @@
                         <span class="navbar-toggler-icon"></span>
                   </button>
                   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                              <li class="nav-item">
-                                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <ul
+                              class="navbar-nav my-lg-0 my-4 me-auto justify-content-lg-between align-items-start"
+                        >
+                              <li class="nav-item mx-lg-5 mx-0">
+                                    <a
+                                          class="nav-link fs-5 mb-0 menu-item-color px-lg-3 px-0 active fw-bold"
+                                          aria-current="page"
+                                          href="#"
+                                          >Home</a
+                                    >
                               </li>
-                              <li class="nav-item">
-                                    <a class="nav-link" href="#">shop</a>
+                              <li class="nav-item mx-lg-5 mx-0">
+                                    <a class="nav-link fs-5 px-lg-3 px-0 mb-0 menu-item-color" href="#"
+                                          >shop</a
+                                    >
                               </li>
-                              <li class="nav-item">
-                                    <a class="nav-link" href="#">blog</a>
+                              <li class="nav-item mx-lg-5 mx-0">
+                                    <a class="nav-link fs-5 px-lg-3 px-0 mb-0 menu-item-color" href="#"
+                                          >blog</a
+                                    >
                               </li>
-                              <li class="nav-item">
-                                    <a class="nav-link" href="#">about</a>
+                              <li class="nav-item mx-lg-5 mx-0">
+                                    <a class="nav-link fs-5 px-lg-3 px-0 mb-0 menu-item-color" href="#"
+                                          >about</a
+                                    >
                               </li>
-                              <li class="nav-item">
-                                    <a class="nav-link" href="#">contact</a>
+                              <li class="nav-item mx-lg-5 mx-0">
+                                    <a class="nav-link fs-5 px-lg-3 px-0 mb-0 menu-item-color" href="#"
+                                          >contact</a
+                                    >
                               </li>
                         </ul>
-                        <form class="d-flex">
-                              <img src="../assets/svg/Search.svg" alt="">
-                              <img src="../assets/svg/Cart.svg" alt="">
+                        <form class="d-flex ms-lg-5 ps-lg-5 ms-0 ps-0">
+                              <a href="#"
+                                    ><img
+                                          src="../assets/svg/Search.svg"
+                                          class="me-3 right-icon-size"
+                                          alt=""
+                              /></a>
+                              <a href="#">
+                                    <img
+                                          src="../assets/svg/Cart.svg"
+                                          class="ms-3 right-icon-size"
+                                          alt=""
+                              /></a>
                         </form>
                   </div>
             </div>
@@ -46,4 +74,12 @@
 export default {};
 </script>
 
-<style></style>
+<style>
+.menu-item-color {
+      color: #1e1e1e !important;
+}
+.right-icon-size {
+      width: 25px !important;
+      height: 25px !important;
+}
+</style>
