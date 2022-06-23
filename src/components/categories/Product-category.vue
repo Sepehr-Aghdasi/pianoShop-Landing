25c767316a730472a170e79f151ebdb8705d1405
Sepehr-Aghdasi82 <Sepehr.Aghdasi82@gmail.com>
2022-06-23 15:45:22 +0430

    Create Catefories component

---
+++ src/components/categories/Product-category.vue
@@ -0,0 +1,33 @@
+<template>
+      <div class="categories-area">
+            <span class="custom-line mx-lg-5 mx-1"></span>
+            <h3 class="text-center m-0">Categories</h3>
+            <span class="custom-line mx-lg-5 mx-1"></span>
+      </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.categories-area {
+      margin: 50px 0;
+      display: flex;
+      justify-content: center;
+      h3 {
+            font-weight: 600;
+            font-size: 38px;
+            line-height: 48px;
+            text-align: center;
+            letter-spacing: 0.02em;
+            overflow: hidden;
+      }
+      span {
+            border-bottom: solid #000 2px;
+            display: flex;
+            width: 15%;
+            margin-bottom: 23px;
+      }
+}
+</style>
