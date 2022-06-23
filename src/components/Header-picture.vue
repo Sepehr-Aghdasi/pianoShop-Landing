4f2f75c8ba50cdc511f464c04e8770b95554504a
Sepehr-Aghdasi82 <Sepehr.Aghdasi82@gmail.com>
2022-06-23 14:50:28 +0430

    Compelete Header picture component

--- src/components/Header-picture.vue
+++ src/components/Header-picture.vue
@@ -1,13 +1,40 @@
 <template>
-  
+      <div class="col-12">
+            <div class="header-image">
+                  <div class="header-pic-text h-100">
+                        <p class="m-0 text-center">Welcome to</p>
+                        <p class="m-0 text-center">Antique Piano Shop</p>
+                  </div>
+            </div>
+      </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style lang="scss">
+.header-image {
+      width: 100%;
+      height: 512px;
+      background-image: url("../assets/images/HeaderPic.png");
+      background-repeat: no-repeat;
+      background-position: center center;
+      -webkit-background-size: cover;
+      background-size: cover;
+      .header-pic-text {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            p {
+                  font-weight: 600;
+                  font-size: 50px;
+                  line-height: 102px;
+                  text-align: center;
+                  letter-spacing: -0.03em;
+                  color: #fff;
+            }
+      }
+}
 </style>
