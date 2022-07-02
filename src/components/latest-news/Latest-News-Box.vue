54b213089a4e0a60d74e6caa4dd10fdfbfc18aaa
Sepehr-Aghdasi82 <Sepehr.Aghdasi82@gmail.com>
2022-07-03 01:28:10 +0430

    Add props to component

--- src/components/latest-news/Latest-News-Box.vue
+++ src/components/latest-news/Latest-News-Box.vue
@@ -1,13 +1,42 @@
 <template>
-  
+      <div class="card bg-black">
+            <a :href="url">
+                  <img
+                        :src="require(`../../assets/images/${imageUrl}`)"
+                        class="card-img-top"
+                        alt=""
+                  />
+            </a>
+            <div class="card-body px-0">
+                  <h5 class="card-title text-muted text-uppercase">{{ title }}</h5>
+                  <p class="card-text fs-5">
+                        {{ description }}
+                  </p>
+            </div>
+      </div>
 </template>
 
 <script>
 export default {
-
-}
+      props: {
+            title: {
+                  type: String,
+                  required: true,
+            },
+            description: {
+                  type: String,
+                  required: true,
+            },
+            imageUrl: {
+                  type: String,
+                  required: true,
+            },
+            url: {
+                  type: String,
+                  required: true,
+            },
+      },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
