f08a441c74e5966a47bd91d70ff16c86b14a8df5
Sepehr-Aghdasi82 <Sepehr.Aghdasi82@gmail.com>
2022-07-04 20:42:56 +0430

    Add footer items

--- src/components/footer/Custom-Footer.vue
+++ src/components/footer/Custom-Footer.vue
@@ -1,13 +1,81 @@
 <template>
-  
+      <div class="custom-footer">
+            <div class="card bg-dark text-white">
+                  <img src="../../assets/images/Piano-Style-Footer.png" class="card-img" alt="" />
+                  <div
+                        class="card-img-overlay d-flex flex-column justify-content-around align-items-center"
+                  >
+                        <h5 class="h2 text-white card-title text-center mb-lg-3 mb-0">ANTIQUE PIANO</h5>
+                        <p class="card-text text-white h1 mb-lg-3 mb-0">We Deliver You The Best Notes.</p>
+                        <div class="col-12">
+                              <div class="footer-links nav justify-content-center">
+                                    <li class="nav-item d-flex">
+                                          <a href="#"><p>about</p> </a>
+                                          <span class="vr"></span>
+                                    </li>
+
+                                    <li class="nav-item d-flex">
+                                          <a href="#"><p>T & CS</p> </a>
+                                          <span class="vr"></span>
+                                    </li>
+
+                                    <li class="nav-item d-flex">
+                                          <a href="#"><p>DELIVERY</p> </a>
+                                          <span class="vr"></span>
+                                    </li>
+
+                                    <li class="nav-item d-flex">
+                                          <a href="#"><p>PRIVACY</p> </a>
+                                          <span class="vr"></span>
+                                    </li>
+
+                                    <li class="nav-item d-flex">
+                                          <a href="#"><p>MY ACCOUNT</p> </a>
+                                          <span class="vr"></span>
+                                    </li>
+
+                                    <li class="nav-item d-flex">
+                                          <a href="#"><p>CONTACT</p> </a>
+                                    </li>
+                              </div>
+                        </div>
+                        <div class="text-center">
+                              <p class="m-0 text-muted">©2022 ANTIQUE PIANO</p>
+                        </div>
+                  </div>
+            </div>
+      </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.custom-footer {
+      .footer-links {
+            p {
+                  padding: 5px !important;
+                  margin: 0 !important;
+                  color: rgba(255, 255, 255, 0.8) !important;
+            }
+            .vr {
+                  width: 2px !important;
+                  color: #fff !important;
+                  background-color: #fff !important;
+                  opacity: 1 !important;
+            }
+      }
+      //       .card-img-overlay {
+      //             background: black;
+      //             overflow: hidden;
+      //             object-fit: cover;
+      //             opacity: 0.3;
+      //             p,
+      //             h5 {
+      //                   opacity: 1 !important;
+      //                   color: #fff !important;
+      //             }
+      //       }
+}
 </style>
